dece06fdc8f33cd8cc962e0d6e50f29527a8c9d0
andrewnelsonnyc@gmail.com <ajnels@DESKTOP-BCQ7725>
2023-11-06 22:22:54 -0500

    removed vestigial main.py

--- main.py
+++ main.py
@@ -1,37 +1,6 @@
-from app import app, db
-from app.db.test_data.data import load_guild
-# Create a command line command to initialize the database
-@app.cli.command('db_create')
-def db_create():
-    with app.app_context():
-        db.create_all()
-        print("Database created!")
+from app.discord_bot.bot import bot
+from app.config import DISCORD_BOT_TOKEN
 
-# Create a command line command to drop the database
-@app.cli.command('db_drop')
-def db_drop():
-    with app.app_context():
-        db.drop_all()
-        print("Database dropped!")
-
-# Create a command line command to seed the database
-@app.cli.command('db_seed')
-def db_seed():
-    with app.app_context():
-        load_guild()
-        
-        print("Database seeded!")
-    
-@app.cli.command('db_reseed')
-def db_reseed():
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-        load_guild()
-        print("Database reseeded!")
-        
 if __name__ == '__main__':
-    app.run(debug=True)
-    # with app.app_context():
-    #     db.create_all()
-    #     print('created database')
+    # Run the bot with your token
+    bot.run(DISCORD_BOT_TOKEN)
--- main_bot.py
+++
@@ -1,6 +0,0 @@
-from app.discord_bot.bot import bot
-from app.config import DISCORD_BOT_TOKEN
-
-if __name__ == '__main__':
-    # Run the bot with your token
-    bot.run(DISCORD_BOT_TOKEN)
