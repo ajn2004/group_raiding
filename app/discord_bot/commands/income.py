22c6565486806ff2ffbaca4aa3c2adabefa303ea
andrewnelsonnyc@gmail.com <ajnels@DESKTOP-BCQ7725>
2023-11-10 02:25:28 -0500

    adjusted rates of income and logic for tracking

--- app/discord_bot/commands/income.py
+++ app/discord_bot/commands/income.py
@@ -25,9 +25,8 @@ class Income(commands.Cog):
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent) -> None:
         if payload.member:
             add_object = {'player':payload.member.id, 'amount' : random.randint(5,15)}
-        if str(payload.emoji) == PDT:
-            add_object['amount'] *= random.randint(2,4)
-        if self.db_controller.add_pdt(add_object=add_object):
-            self.db_controller.track_usage('',user=add_object['player'],  command = f'income of {add_object["amount"]} pdt for reaction')
-
-    
+            if str(payload.emoji) == PDT:
+                add_object['amount'] *= random.randint(2,4)
+                print('reaction',add_object)
+            if self.db_controller.add_pdt(add_object=add_object):
+                self.db_controller.track_usage('',user=payload.member.id,  command = f'income of {add_object["amount"]} pdt for reaction') 
