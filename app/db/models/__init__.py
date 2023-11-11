55057a0e36d9fe0c8cde81fc86170cbe8a613429
andrewnelsonnyc@gmail.com <ajnels@DESKTOP-BCQ7725>
2023-11-11 10:10:29 -0500

    Made modifications to alembic version and models init.py

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -6,3 +6,6 @@ from .role import Role
 from .schedule import Schedule
 from .specialization import Specialization
 from .usage import Usage
+from .bet import Bet
+from .betEvent import BetEvent
+from .betOutcome import BetOutcome
